afaf10381afdd629684ec61e0924dabb472a35f0
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-03-31 20:53:23 +0530

    Add files via upload

---
+++ Assignment/Practical 4/Experiment 4.14.py
@@ -0,0 +1,5 @@
+# display 1 to 10 in reverse order
+print("Name: Agamjot Singh\nRoll No: 2210997017")
+
+for i in range(10, 0, -1):
+    print(i)
---
+++ Assignment/Practical 4/Experiment 4.2.py
@@ -0,0 +1,7 @@
+#WAP to show working of a while Loop with a Numbers
+print("Name: Agamjot Singh\nRoll No: 2210997017")
+
+a = 0
+while a <= 10:
+    print(a)
+    a = a + 1
