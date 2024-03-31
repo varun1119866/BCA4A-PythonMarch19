32add545ee666c538131c1750cbcc103ddb00385
Khushi <127961906+Khushi271@users.noreply.github.com>
2024-03-31 21:25:51 +0530

    Update 3.1.py

--- New folder/3.1.py
+++ New folder/3.1.py
@@ -1,4 +1,4 @@
-print("NAME : RAGHAV \n ROLL NO : 2210997182")
+print("NAME : KHUSHI \n ROLL NO : 2210997120")
 a=int(input("Enter First Number : "))
 b=int(input("Enter Second Number : "))
 if a%b==0:
