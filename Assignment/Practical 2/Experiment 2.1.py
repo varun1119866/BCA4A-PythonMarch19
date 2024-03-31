7fc76610ccd009aae9efe2f8ad8bdc6784608a02
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2024-03-31 21:02:15 +0530

    Add files via upload

---
+++ Assignment/Practical 2/Experiment 2.1.py
@@ -0,0 +1,11 @@
+#WAP to demonstrate Slicing Operations in Strings
+print("Name: Drishti Gupta\nRoll No: 2210997286")
+
+
+str = "Drishti Gupta"
+print(str[0:8])
+print(str[8:13])
+print(str[0:13])
+
+print(str[0:13:2])
+print(str[0:13:4])
