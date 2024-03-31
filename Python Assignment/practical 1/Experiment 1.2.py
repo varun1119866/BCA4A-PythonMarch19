411029ca7540da300ac7707650ca1966f0fb15aa
Dhruv071 <127030697+Dhruv071@users.noreply.github.com>
2024-03-31 15:29:10 +0530

    Add files via upload

---
+++ Python Assignment/practical 1/Experiment 1.2.py
@@ -0,0 +1,8 @@
+#WAP to calculate Avg. marks of 3 subjects
+print("Name: Dhruv \nRoll No: 2210997071\n")
+
+a = int(input("Enter the marks of subject 1: "))
+b = int(input("Enter the marks of subject 2: "))
+c = int(input("Enter the marks of subject 3: "))
+avg = (a+b+c)/3
+print("The average marks of three subjects are ", avg)
