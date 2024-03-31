b8f8cd425586bcb632729e7dbd0fbc179a9d72d2
akangsha2312 <126556906+akangsha2312@users.noreply.github.com>
2024-03-31 15:31:54 +0530

    Add files via upload

---
+++ Experiment 2.2.py
@@ -0,0 +1,12 @@
+#WAP to demonstrate built in functions of Strings
+print("Name: Akangsha\nRoll No: 2210997020")
+
+str = "Akangsha"
+print(str)
+print(str.upper())
+print(str.lower())
+print(str.split())
+print(str.strip())
+print(str.count("a"))
+print(str.find("a"))
+print(str.replace("a", "e"))
