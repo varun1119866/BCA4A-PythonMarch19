9ef447d0e7a08b57d2482e839c2d4840b1ed75b2
Nishant Rana <126556240+Nishant-Rana1@users.noreply.github.com>
2024-03-31 17:46:31 +0530

    Add files via upload

---
+++ Assignment/Practical 4/Experiment 4.11.py
@@ -0,0 +1,9 @@
+# factorial of a number
+print("Name: Nishant Rana\nRoll No: 2210997163")
+num = int(input("Enter a number: "))
+fact = 1
+i = 1
+while i <= num:
+    fact = fact * i
+    i = i + 1
+print(f"Factorial of {num} is {fact}")
---
+++ Assignment/Practical 4/Experiment 4.2.py
@@ -0,0 +1,6 @@
+#WAP to show working of a while Loop with a Numbers
+print("Name: Nishant Rana\nRoll No: 2210997163")
+a = 0
+while a <= 10:
+    print(a)
+    a = a + 1
