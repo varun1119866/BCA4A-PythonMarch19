8adbc2519650412a06d7f8c9bc7bf3b1eee8af8b
Dhruv071 <127030697+Dhruv071@users.noreply.github.com>
2024-03-31 15:37:49 +0530

    Create Experiment 2.1.py

---
+++ Python Assignment/practical 2/Experiment 2.1.py
@@ -0,0 +1,10 @@
+#WAP to demonstrate Slicing Operations in Strings
+print("Name: Dhruv \nRoll No: 2210997071\n")
+
+str = "this is a python program"
+print(str[0:8])
+print(str[8:13])
+print(str[0:13])
+
+print(str[0:13:2])
+print(str[0:13:4])
