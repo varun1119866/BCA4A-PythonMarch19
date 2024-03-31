3d382acf5562b0bdf940db9b299068755d80ab68
Dev <157810256+devbatra2004@users.noreply.github.com>
2024-03-31 16:08:13 +0530

    Python Assignment(Dev_2210997068)

---
+++ DEV_2210997068_BCA4B/practical-2.1.py
@@ -0,0 +1,6 @@
+#2.1 WAP to demonstrate Slicing Operations in Strings
+print("name-dev,rollno-2210997068")
+string = "Hello, World!"
+print(string[0:5])  
+print(string[7:])   
+print(string[::-1]) 
---
+++ DEV_2210997068_BCA4B/practical-2.2.py
@@ -0,0 +1,7 @@
+#WAP to demonstrate built in functions of Strings
+print("name-dev,rollno-2210997068")
+string = "Hello, World!"
+print(len(string))       
+print(string.upper())   
+print(string.lower())   
+print(string.split(",")) 
