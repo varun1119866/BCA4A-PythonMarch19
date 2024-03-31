015283448c1962b84a2cef6975a76a058132e00d
Khushi <127961906+Khushi271@users.noreply.github.com>
2024-03-31 21:34:27 +0530

    Update 5.2.py

--- New folder/5.2.py
+++ New folder/5.2.py
@@ -1,4 +1,4 @@
-print("NAME : RAGHAV \n ROLL NO : 2210997182")
+print("NAME : KHUSHI \n ROLL NO : 2210997120")
 # Display the cube of the first 10 even numbers
 print("Cube of the first 10 even numbers:")
 for i in range(2, 21, 2):
