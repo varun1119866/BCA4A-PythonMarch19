141d634434dd926e060f636e61ce4cd2e2e748fb
Dhruv071 <127030697+Dhruv071@users.noreply.github.com>
2024-03-31 15:48:34 +0530

    Update Experiment 5.1.py

--- Python Assignment/practical 5/Experiment 5.1.py
+++ Python Assignment/practical 5/Experiment 5.1.py
@@ -3,7 +3,7 @@ print("Name: Dhruv \nRoll No: 2210997071\n")
 
 list1 = []
 list1.append(1)
-list1.append("Agamjot")
+list1.append("Dhruv")
 list1.append(3.45)
 list1.append(True)
 list1.append([1,2,3,4,5])
