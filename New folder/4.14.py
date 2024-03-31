004174fa712f467158e6cf0ea8e9a8695a1c92bf
Khushi <127961906+Khushi271@users.noreply.github.com>
2024-03-31 21:30:12 +0530

    Update 4.14.py

--- New folder/4.14.py
+++ New folder/4.14.py
@@ -1,4 +1,4 @@
-print("NAME : RAGHAV \n ROLL NO : 2210997182")
+print("NAME : KHUSHI \n ROLL NO : 2210997120")
 # Display the numbers from 10 to 1 in reverse order
 print("Numbers from 10 to 1 in reverse order:")
 for i in range(10, 0, -1):
